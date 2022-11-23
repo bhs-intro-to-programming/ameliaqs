6978dbf06cee3fcfddde24ac5e13e60ad1ce1c00
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2022-11-22 20:16:45 -0800

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -20,4 +20,4 @@ const isFlobbyBird = (red, spotted) => red && spotted
 const isBloggyBird = (red, spotted) => red && !spotted
 const isFlibbleBird = (red, spotted) => !red && spotted
 const isGlobbyBird = (red, spotted) => !red && !spotted
-const eatsWorms = (isGlobbyBird) => !isGlobbyBird
+const eatsWorms = (red, spotted) => red || spotted
