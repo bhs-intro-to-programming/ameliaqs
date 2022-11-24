32c1cf31a6b4ed53e252f19befb1086e89f25fdd
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2022-11-23 18:29:23 -0800

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -32,4 +32,4 @@
 // Feel free to play around in the REPL to make sure you understand how this
 // operator works.
 const pigLatin = (s) => s.substring((s.search(/[aeiou]/))) + s.substring(0,s.search(/[aeiou]/)) + 'ay'
-const advancedPigLatin = (s) => s.search(/[aeiou]/) !== 0 ? s.substring((s.search(/[aeiou]/))) + s.substring(0,s.search(/[aeiou]/)) + 'ay' : s + 'ay'
+const advancedPigLatin = (s) => s.search(/[aeiou]/) !== 0 ? s.substring((s.search(/[aeiou]/))) + s.substring(0,s.search(/[aeiou]/)) + 'ay' : s + 'way'
