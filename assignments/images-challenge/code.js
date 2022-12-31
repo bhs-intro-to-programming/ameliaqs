2454350827962797f9dae309730b399585d59f3f
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2022-12-30 21:23:27 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -42,7 +42,6 @@ const lineOfCircles = (r) => {
 // lineOfCircles(44)
 
 const alternatingCircles = (r) => {
-  const blueCircles
     const numCircles = Math.floor(width / (2 * r));
     const numBlueCircles = 1 + Math.floor(width / (2 * r)) / 2;
     const totalWidth = numCircles * 2 * r;
