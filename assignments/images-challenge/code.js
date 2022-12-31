2ff1e48edbe3e9c181ad1bcc230347a351c60a1d
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2022-12-30 21:31:26 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -50,10 +50,7 @@ const alternatingCircles = (r) => {
   for (let i = 0; i < numCircles; i++) {
     drawFilledCircle(gap + r + (i * 4 * r), height / 2, r, 'blue')
   }
-  const numCircles = Math.floor(width / (2 * r));
   const numBlueCircles = Math.floor(width / (2 * r)) / 2;
-  const totalWidth = numCircles * 2 * r;
-  const gap = (width - totalWidth) / 2;
   for (let i = 0; i < numCircles; i++) {
     drawFilledCircle(gap + r + (i * 4 * r), height / 2, r, 'red')
   }
