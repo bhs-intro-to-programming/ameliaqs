c99f9259cb4568535f4b0092c7bd53ae9532fe59
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2022-12-30 20:11:44 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -23,11 +23,22 @@
  *
  *  clear()
  */
-const lineOfCircles = (r) => {
+const XlineOfCircles = (r) => {
   const numCircles = Math.floor(width / (2 * r));
   const totalWidth = numCircles * 2 * r;
   const gap = (width - totalWidth) / 2;
   for (let x = gap + r; x <= (width - gap) - r; x += 2 * r)
     drawFilledCircle(x, height / 2, r, 'red')
 }
+
+const lineOfCircles = (r) => {
+  const numCircles = Math.floor(width / (2 * r));
+  const totalWidth = numCircles * 2 * r;
+  const gap = (width - totalWidth) / 2;
+  for (let i = 0; i < numCircles; i++) {
+    drawFilledCircle(gap + r + (i * 2 * r), height / 2, r, 'red')
+  }
+}
+
+
 lineOfCircles(44)
