0eb2629f20ccbd762fd6fdf807070e7df6002607
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2022-12-31 16:45:26 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -64,7 +64,7 @@ const concentricCircles = (n) => {
   const difference = MaxR / n;
   for (let i = 0; i < n; i++) {
     if ((i % 2) === 0) {
-      drawFilledCircle(width / 2, height / 2, MaxR - difference * i, 'mauve');
+      drawFilledCircle(width / 2, height / 2, MaxR - difference * i, 'blue');
     } else {
       drawFilledCircle(width / 2, height / 2, MaxR - difference * i, 'red');
     }
