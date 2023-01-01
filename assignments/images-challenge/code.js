ab6b63d1764a804fe5f6b47e0b860bf8ab51c23f
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2022-12-31 22:57:20 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -108,7 +108,7 @@ const notReallyCurved = (n) => {
   const difference = Math.min(width, height) / n
   const frameSide = difference * n
   for (let i = 0; i < n; i++) {
-    drawLine(0, 0 + i * difference, frameSide - i * difference, frameSide, 'black', 1)
+    drawLine(0, 0 + i * difference, 0 + i * difference, frameSide, 'black', 1)
   }
 }
 notReallyCurved(10)
