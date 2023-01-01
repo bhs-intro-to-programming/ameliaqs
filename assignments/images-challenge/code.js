70a00c656a8862e4e02c14424d3cb123d50edfa6
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-01-01 10:47:32 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -143,4 +143,15 @@ const fillWithCirlcesRandomlyFilled = (s, p) => {
       // }
     }
 }
-fillWithCirlcesRandomlyFilled(17, 0.23)
+// fillWithCirlcesRandomlyFilled(17, 0.23)
+
+const squareOfCirlces = (s) => {
+  const d = 2 * s
+  const numCircles = Math.min(width, height) / d
+  const left = (width - numCircles * d) / 2
+  const top = (height - numCircles * d) / 2
+  for (let c = 0; c < numCirlces; c++) {
+    drawCircle(s + left + c * d, s + top + r * d, s, 'blue', 1);
+  }
+}
+squareOfCircles (10)
