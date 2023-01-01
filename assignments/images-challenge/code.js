f12f41540c2b49df8b7f04745395db3811f96a59
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2022-12-31 17:31:36 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -81,11 +81,10 @@ const checkerboard = (n) => {
       }
     }
     else {
-      for (let c = 0; c < n; c++) {
         const color = (c % 2) === 0 ? 'blue' : 'red';
         drawFilledRect(gap + side * c, 0 + side * r, side, side, 'color')
 
-      }
+    
     }
   }
 }
