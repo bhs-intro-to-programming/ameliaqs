662b23642e7235b8e80b3b6cb467d67f436bf7ed
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-01-01 17:34:17 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -192,7 +192,7 @@ const squareOfCircles = (s) => {
   const gap = (numCircles - 1) * d
   for (let r = 0; r < numCircles; r++)
     for (let c = 0; c < numCircles; c++) {
-      const x = 0 < r && r < numCircles - 1? s + left + c * gap : s + left + c * d;
+      const x = r > 0 && r < numCircles - 1? s + left + c * gap : s + left + c * d;
       drawCircle(x, s + top + r * d, s, 'blue', 1);
     }
 }
