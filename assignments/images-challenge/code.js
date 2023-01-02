a940a1796cceffacf2a1bb951d1fa0d9f0405866
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-01-01 16:44:42 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -171,8 +171,8 @@ const squareOfCircles = (s) => {
         drawCircle(s + left + c * d, s + top + r * d, s, 'blue', 1)
       }
     } else {
-      for (let x = 0; x <= 2; x++) {
-        drawCircle(s + left + d * x, s + top + r * d, s, 'blue', 1)
+      for (let c = 0; c <= 2; c++) {
+        drawCircle(s + left + c * gap, s + top + r * d, s, 'blue', 1)
       }
     }
   }
