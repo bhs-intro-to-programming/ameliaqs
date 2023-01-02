4af08b13ea3d607762f173371602d86ac2db1290
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-01-01 17:51:31 -0800

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -161,7 +161,7 @@ const XsquareOfCircles = (s) => {
 
 const squareOfCircles = (s) => {
   const d = 2 * s;
-  const numCircles = Math.min(width, height) / d;
+  const numCircles = Math.floor(Math.min(width, height) / d);
   const left = (width - numCircles * d) / 2;
   const top = (height - numCircles * d) / 2;
   const gap = (numCircles - 1) * d;
