cefd46cc37832f804501aabc2c2d72dd15d5cca4
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-01-02 17:28:04 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -34,7 +34,7 @@ const anyOverOneHundred = (x) => {
 
 const pyramid = (x) => {
   let newArray = []
-  for (let j = 1; j < x; j++) {
+  for (let j = 1; j <= x; j++) {
     for (let i = 0; i < x; i++) {
       newArray.push(j)
     }
