473e093f4fd734db8d71eac8c8a0ccd1d6249a40
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-01-02 18:57:25 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -6,3 +6,4 @@ const stringContains = (s, t) => {
 }
 const firstAndLast = (s) => s[0] + s[s.length - 1]
 const swapFrontAndBack = (s) => s.substring(Math.floor(s.length / 2)) + s.substring(0, Math.floor(s.length / 2))
+const simplePigLatin = (s, n) => s.substring(n + 1) + 'ay'
