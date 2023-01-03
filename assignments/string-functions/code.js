de3630f6fd5d9b92fb982dfa2aeba4617116201b
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-01-02 17:42:36 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -1,9 +1,8 @@
 const findFnord = (s) => {
   return s.indexOf('fnord')
 }
-
 const stringContains = (s, t) => {
   return s.indexOf(t) > -1
 }
-
 const firstAndLast = (s) => s[0] + s[s.length - 1]
+const swapFrontAndBack = (s) => s.substring(0, Math.floor(s.length / 2) + 1) + s.substring(Math.floor(s.length / 2) + 1)
