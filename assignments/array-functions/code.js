06d8869012651d8dbe45c68246d167a756070a66
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-01-02 17:00:10 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -2,7 +2,14 @@ const countTens = (x) => {
   let count = 0
   for (let i = 0; i < x.length; i++) {
     if (x[i] === 10)
-    count++
+      count++
   }
   return count
+}
+const sum = (x) => {
+  let sum = 0
+  for (let i = 0; i < x.length; i++) {
+    sum = sum + x[i]
+  }
+  return sum
 }
