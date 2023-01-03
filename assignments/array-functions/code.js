acda2a87344b4dbcba56933bdb0880cb6df8915e
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-01-02 17:02:51 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -6,10 +6,20 @@ const countTens = (x) => {
   }
   return count
 }
+
 const sum = (x) => {
   let sum = 0
   for (let i = 0; i < x.length; i++) {
     sum = sum + x[i]
   }
   return sum
+}
+
+const evens = (x) => {
+  let newArray = []
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] % 2 === 0)
+    newArray.push(x[i]) 
+  }
+  return newArray
 }
