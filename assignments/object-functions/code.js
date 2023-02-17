1b108b56256fa11b271a858eddcd75cd53f52736
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-02-16 21:52:42 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -7,6 +7,6 @@ const emptyObject = () => {
   let emptyObject = {}
   return emptyObject
 }
-const distance = (point, point) = {
-  return Math.sqrt((point.x - point.x)**2 + (point.y - point.y)**2) 
+const distance = (point) = {
+  return point.x
 }
