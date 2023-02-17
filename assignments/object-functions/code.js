9052ce9ca5c9b9a3f614cdabf27c0ee3d9e4d949
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-02-16 21:42:15 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -1,5 +1,9 @@
 const getX = (object) => { return object.x }
-const point = (x, y) => { 
-  let point = {'x' : x, 'y': y}
+const point = (x, y) => {
+  let point = { 'x': x, 'y': y }
   return point
+}
+const emptyObject = () => {
+  let emptyObject = {}
+  return emptyObject
 }
