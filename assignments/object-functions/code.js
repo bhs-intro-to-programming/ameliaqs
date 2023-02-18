211f6977247a39d5eec07d63f93493ac95b3a765
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-02-18 07:09:54 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -23,7 +23,7 @@ const sumSalaries = (x) => {
 const newHighScore = (current, scores) => {
   let all = []
   for (let i = 0; i < scores.length; i++) {
-    all.push(i)
+    all.push(scores[0 + i]['score'])
   }
   return all
 }
