a16641e6e51faf7494e519a71764ec687e72a49a
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-03-06 07:40:22 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -13,3 +13,7 @@ for (let i = 1; i <= 3; i++) {
   drawLine(i * width / 3, 0 , i * width / 3, height, 'black', 1)
   drawLine(0, i * height / 3, width, i * height / 3, 'black', 1)
 }
+
+registerOnclick((x, y) => {
+ drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
+});
