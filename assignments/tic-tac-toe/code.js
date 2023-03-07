5580aa74f4f003b7b627f8d79a28f5f606c926b4
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-03-06 20:44:06 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -24,7 +24,7 @@ registerOnclick((x, y) => {
 let c = Math.floor(x / (width / 3))
 let xx = c * (width / 3) + (width / 6) - (width / 20)
 let r = Math.floor(y / (height / 3))
-let yy = r * (height / 3) + (height / 6)
+let yy = r * (height / 3) + (height / 6) + height / 20
   if (i % 2 == 0) {
     drawText('X', xx, yy, 'black', Math.min(width, height) * 0.3);
     i++
