5855db849cf0bf518c9db38c6e1f6ad14b61a837
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-03-21 07:44:31 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -27,10 +27,10 @@ const columnWinner = (c) => {
   return board[0][c] === board[1][c] && board[0][c] === board[2][c]
 }
 const diagonalWinnerA = (r, c) => {
-  return board[r][c]!== "" && board[0][0] === board[1][1] && board[0][0] === board[2][2]
+  return board[0][0] === board[1][1] && board[0][0] === board[2][2]
 }
 const diagonalWinnerB = (r, c) => {
-  return board[r][c]!== "" && board[2][0] === board[1][1] && board[2][0] === board[0][2]
+  return board[2][0] === board[1][1] && board[2][0] === board[0][2]
 }
 
 let i = 0
