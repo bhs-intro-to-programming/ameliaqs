f43f89684580d9d8b510f646011e30cf572cb1c1
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-04-03 21:30:02 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -24,6 +24,6 @@ let j = 0
 for (j = 0; j < 8; j++) {
   for (i = 0; i < 8; i++) {
     const color = (i % 2) === 0 ? 'beige' : 'black'
-    drawFilledRect(sideGap + (s * i), topGap, (s + 1), s, color)
+    drawFilledRect(sideGap + (s * i), topGap, (s + 1) + 100, s, color)
   }
 }
