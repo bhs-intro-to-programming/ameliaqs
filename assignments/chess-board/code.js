f3c8c94ca3be8b0f376097689dfb024f8110fbb0
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-04-03 21:15:28 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -21,6 +21,6 @@ const topGap = (height - board) / 2
 
 let i = 0
 for (i = 0; i < 4; i++) {
-  drawFilledRect(sideGap + i * square, topGap, 2 * i * square, square, 'white')
+  drawFilledRect(sideGap + i * square, topGap, (i + 1) * square, square, 'white')
 }
 
