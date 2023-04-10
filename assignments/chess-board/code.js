8f78254a4d62b772c5ec6237de5abbcbf3802dc9
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-04-09 18:24:42 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -12,7 +12,7 @@ const BLACK_KNIGHT = '♞';
 const BLACK_PAWN = '♟';
 
 // Example of drawing one of the pieces
-//drawText(WHITE_KING, width / 2, height / 2, 'black', 64);
+drawText(WHITE_KING, width / 2, height / 2, 'black', 64);
 
 const board = Math.min(width, height)
 const s = board / 8
