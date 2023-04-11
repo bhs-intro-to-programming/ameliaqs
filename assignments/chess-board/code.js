1c2fee39898591574d26d2c6e0ecf5c59932b093
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-04-10 20:23:31 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -46,7 +46,8 @@ registerOnclick((x, y) => {
   let highlightWidth = s / 2
   if (pieces[r][c] = BLACK_PAWN) {
     drawCircle(sideGap + (s * c) + highlightWidth, topGap + (s * (r + 1)) + highlightWidth, s / 2, 'green', 1)
-    console.log(pieces[r][c])
+  
   }
+    console.log(pieces[r][c])
   
 })
