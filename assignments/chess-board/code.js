afe1815e99fb55d53d5b6611d190021bccf6385b
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-06-01 20:27:08 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -3,18 +3,18 @@ const piece = (kind, color, icon) => {
   return { kind, color, icon };
 };
 
-const WHITE_KING   = piece('king', 'white', '♔');
-const WHITE_QUEEN  = piece('queen', 'white', '♕');
-const WHITE_ROOK   = piece('rook', 'white', '♖');
+const WHITE_KING = piece('king', 'white', '♔');
+const WHITE_QUEEN = piece('queen', 'white', '♕');
+const WHITE_ROOK = piece('rook', 'white', '♖');
 const WHITE_BISHOP = piece('bishop', 'white', '♗');
 const WHITE_KNIGHT = piece('knight', 'white', '♘');
-const WHITE_PAWN   = piece('pawn', 'white', '♙');
-const BLACK_KING   = piece('king', 'black', '♚');
-const BLACK_QUEEN  = piece('queen', 'black', '♛');
-const BLACK_ROOK   = piece('rook', 'black', '♜');
+const WHITE_PAWN = piece('pawn', 'white', '♙');
+const BLACK_KING = piece('king', 'black', '♚');
+const BLACK_QUEEN = piece('queen', 'black', '♛');
+const BLACK_ROOK = piece('rook', 'black', '♜');
 const BLACK_BISHOP = piece('bishop', 'black', '♝');
 const BLACK_KNIGHT = piece('knight', 'black', '♞');
-const BLACK_PAWN   = piece('pawn', 'black', '♟');
+const BLACK_PAWN = piece('pawn', 'black', '♟');
 
 
 
@@ -29,7 +29,7 @@ const sideGap = (width - board) / 2
 const topGap = (height - board) / 2
 
 // draw board and pieces
-const pieces = Array(8).fill().map(() => Array(8).fill(''))
+const pieces = Array(8).fill().map(() => Array(8).fill())
 let row0 = [BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK]
 let row7 = [WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK]
 for (let i = 0; i < 8; i++) {
@@ -45,7 +45,9 @@ const drawBoard = () => {
     for (let c = 0; c < 8; c++) {
       const color = (r + c) % 2 === 0 ? 'beige' : 'black'
       drawFilledRect(sideGap + (s * c), topGap + (s * r), s, s, color)
-      drawText(pieces[r][c].icon, sideGap + (s * c), topGap + (s * (r + 1)), 'grey', 64)
+      if (pieces[r][c] !== undefined) {
+        drawText(pieces[r][c].icon, sideGap + (s * c), topGap + (s * (r + 1)), 'grey', 64)
+      }
     }
   }
 }
