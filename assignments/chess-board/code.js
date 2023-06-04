33c29302aa83fa1f62da7d1e81dfe3655894068b
ameliaqs <118628689+ameliaqs@users.noreply.github.com>
2023-06-03 21:50:27 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -131,7 +131,7 @@ const queenMove = (move, selected) => {
 }
 
 const kingMove = (move, selected) => {
-  if (manhattanDistance(move, selected) === 1 || manhattanDistance(move, selected) === 2 && bishopMove) {
+  if (manhattanDistance(move, selected) === 1 || (bishopMove && manhattanDistance(move, selected) === 2)) {
     return true
   } else {
     return false
